ea4914e1186f73ccd4f5824a761d0f7f920ff3cd
james9001 <james9001@james9001.net>
2024-04-18 23:19:17 +1000

    Add prisma migrate deploy into the entrypoint script, as well as some comment about why

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+#Normally, I would not recommend you run a prisma migrate (or equivalent) here.
+#Such things should be in its own separate one-off task, run only when migrations actually exist.
+#However, for this abridged local stack demo it is fine since it appears to be idempotent in this case.
+npx prisma migrate deploy
+
 npm start
